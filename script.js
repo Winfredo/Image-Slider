bc97d9151b5083d49afdcc3a3a65c585d9f9ec2c
Winfredo <winfrednukpe2002@gmail.com>
2023-03-08 16:11:16 +0000

    commit

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const buttons = document.queryAll(".btn")
-const imgDiv = document.querySelector(".main-container")
+const pictures = [
+  "images/pic1",
+  "images/pic2",
+  "images/pic3",
+  "images/pic4",
+  "images/pic5",
+  "images/pic6",
+];
 
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
+const imgDiv = document.getElementById("main-container");
 
-function randomPic (){
-    const pictures = [
-        "images/pic1",
-        "images/pic2",
-        "images/pic3",
-        "images/pic4",
-        "images/pic5",
-        "images/pic6",
-    ]
-} 
+let counter = 0;
 
-buttons.forEach(function (button) {
-    button.addEventListener("click", function (e){
-        if (button.classList.contains('btn-left')){
-            counter--
-            if(counter < 0){
-                counter = pictures.length - 1
-            }
-            imgDiv.style.background = `url/${pictures[counter].jpeg}`
-        }
+btnLeft.addEventListener("click", function () {
+  console.log("left");
+  counter--;
+  if (counter < 0) {
+    counter = pictures.length - 1;
+  }
+  imgDiv.style.background = `url(./${pictures[counter]}.jpg) center/cover fixed no-repeat`;
+});
 
-        if (button.classList.contains('btn-left')){
-            counter--
-            if(counter < 0){
-                counter = pictures.length - 1
-            }
-            imgDiv.style.background = `url/${pictures[counter].jpeg}`
-    })
-})
+btnRight.addEventListener("click", function () {
+  console.log("Right");
+  counter++;
+  if (counter > pictures.length - 1) {
+    counter = 0;
+  }
+  imgDiv.style.background = `url(./${pictures[counter]}.jpg) center/cover fixed no-repeat`;
+});
